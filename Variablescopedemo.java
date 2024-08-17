03c007ec1d30e1f884e07ebb8238cdfd992d3bd1
savalam-purnasai <151706316+savalam-purnasai@users.noreply.github.com>
2024-08-17 12:23:43 +0530

    Create Variablescopedemo.java

---
+++ Variablescopedemo.java
@@ -0,0 +1,12 @@
+import java.io.*;
+public class Variablescopedemo 
+{
+public static void main(String args[])
+ {
+  for(int x=0; x<5; x++)
+   {
+	 System.out.println("Hello World");
+   }
+    X=10;
+   }
+  }
