d0f53ab8939ebbe48672e01fdc6e621a0cdecd45
savalam-purnasai <151706316+savalam-purnasai@users.noreply.github.com>
2024-08-22 11:51:45 +0530

    Create Dowhiledemo.java

---
+++ Dowhiledemo.java
@@ -0,0 +1,12 @@
+import java.io.*;
+class Dowhiledemo
+{
+    public static void main(String args[])
+    {
+        int i=0;
+        do{
+            System.out.println("Hello world!");
+            i++;
+         }while(i<3);
+    }
+}
