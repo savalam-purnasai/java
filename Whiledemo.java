c604ba28effaef57c892a1cd08ae25a24a739a7d
savalam-purnasai <151706316+savalam-purnasai@users.noreply.github.com>
2024-08-22 12:12:42 +0530

    Create Whiledemo.java

---
+++ Whiledemo.java
@@ -0,0 +1,13 @@
+import java.io.*;
+class   whiledemo
+{
+    public static void main(String args[])
+    {
+        int i=0;
+        while(i<3)
+        {
+            System.out.println("Hello world");
+            i++;
+        }
+    }
+}
